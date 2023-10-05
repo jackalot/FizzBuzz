54f50304eb484b73f97c455884109855f58c850c
jackalot <68657425+jackalot@users.noreply.github.com>
2023-10-05 15:23:06 -0400

    created for loop
    added for loop, added a  new string

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 
 public class Main {
- public static void main(String[] args)
- {
-	 
- }
+
+	public static void main(String[] args)
+	{
+		String newString = "";
+		for(int i = 1; i < 100; i++)
+		{
+			
+		}
+	}
 }
