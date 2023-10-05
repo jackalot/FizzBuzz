85de851992b5515050adf9033f7d17caa1bf1afe
jackalot <68657425+jackalot@users.noreply.github.com>
2023-10-05 15:26:37 -0400

    added if statements
    added if statements that check division with no remainders. adds fizz or buzz depending on which iteration it is.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 
 public class Main {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		String newString = "";
-		for(int i = 1; i < 100; i++)
-		{
-			
+		for (int i = 1; i < 100; i++) {
+			newString = "";
+			if (i % 3 == 0) {
+				newString += "Fizz";
+			}
+			if (i % 5 == 0) {
+				newString += "Buzz";
+			}
+			if (newString == "") {
+				newString = "" + i;
+			}
+			System.out.println(newString);
 		}
 	}
 }
